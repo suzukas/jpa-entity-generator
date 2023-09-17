42a6eee44fafcf0a79646071e58ad35e30339575
Shigeru SUZUKA <shige.pc@gmail.com>
2023-09-17 16:07:00 +0900

    Add support for new MySQL driver.
    Fixed to use catalog obtained from Connection.

--- src/main/java/com/smartnews/jpa_entity_generator/metadata/TableMetadataFetcher.java
+++ src/main/java/com/smartnews/jpa_entity_generator/metadata/TableMetadataFetcher.java
@@ -29,9 +29,10 @@ public class TableMetadataFetcher {
 
     public List<String> getTableNames(JDBCSettings jdbcSettings) throws SQLException {
         DatabaseMetaData databaseMeta = getMetadata(jdbcSettings);
+        String catalog = databaseMeta.getConnection().getCatalog();
         try {
             List<String> tableNames = new ArrayList<>();
-            try (ResultSet rs = databaseMeta.getTables(null,  jdbcSettings.getSchemaPattern(), "%", TABLE_TYPES)) {
+            try (ResultSet rs = databaseMeta.getTables(catalog,  jdbcSettings.getSchemaPattern(), "%", TABLE_TYPES)) {
                 while (rs.next()) {
                     tableNames.add(rs.getString("TABLE_NAME"));
                 }
@@ -51,8 +52,9 @@ public class TableMetadataFetcher {
         tableInfo.setName(table);
         tableInfo.setSchema(Optional.ofNullable(schema));
         DatabaseMetaData databaseMeta = getMetadata(jdbcSettings);
+        String catalog = databaseMeta.getConnection().getCatalog();
         try {
-            try (ResultSet rs = databaseMeta.getTables(null, schema, table, TABLE_TYPES)) {
+            try (ResultSet rs = databaseMeta.getTables(catalog, schema, table, TABLE_TYPES)) {
                 if (rs.next()) {
                     tableInfo.setDescription(Optional.ofNullable(rs.getString("REMARKS")));
                 }
@@ -61,12 +63,12 @@ public class TableMetadataFetcher {
             }
 
             final List<String> primaryKeyNames = new ArrayList<>();
-            try (ResultSet rs = databaseMeta.getPrimaryKeys(null, schema, table)) {
+            try (ResultSet rs = databaseMeta.getPrimaryKeys(catalog, schema, table)) {
                 while (rs.next()) {
                     primaryKeyNames.add(rs.getString("COLUMN_NAME"));
                 }
             }
-            try (ResultSet rs = databaseMeta.getColumns(null, schema, table, "%")) {
+            try (ResultSet rs = databaseMeta.getColumns(catalog, schema, table, "%")) {
                 while (rs.next()) {
                     Column column = new Column();
                     column.setName(rs.getString("COLUMN_NAME"));
